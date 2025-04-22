c55c2e5ff5f15ce519f353ebd2ae8dd212df5eb7
javeed512 <javeedmohammedjaveed@gmail.com>
2025-04-22 17:10:20 +0530

    JS Events

---
+++ JavaScript/eventsDemo.js
@@ -0,0 +1,24 @@
+
+
+
+  function    func1(){
+
+
+    alert("button is clicked..");
+
+
+  }
+
+
+  function  changeColor(){
+
+
+          var  h1Element =  document.getElementById("hid");
+
+
+            h1Element.style = "color:red";
+
+
+
+
+  }
---
+++ JavaScript/functionsExample.js
@@ -0,0 +1,20 @@
+
+
+
+function   add(x,y){
+
+    var i = 0;
+
+    return x+y;
+
+}
+
+
+var  f1 = ()=>{
+
+
+    alert("This is arrow func / anonymous func");
+
+}
+
+
